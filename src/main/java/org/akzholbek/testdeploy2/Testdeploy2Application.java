746c70d4e04d83c47669c35f39f8f3e8086d954f
Akzholbek Abdikarim uulu <akzholbek0096@gmail.com>
2025-01-18 21:16:13 +0600

    test deploy

---
+++ src/main/java/org/akzholbek/testdeploy2/Testdeploy2Application.java
@@ -0,0 +1,13 @@
+package org.akzholbek.testdeploy2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Testdeploy2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Testdeploy2Application.class, args);
+	}
+
+}
---
+++ src/test/java/org/akzholbek/testdeploy2/Testdeploy2ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.akzholbek.testdeploy2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Testdeploy2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
